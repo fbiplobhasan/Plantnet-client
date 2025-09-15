992a65da0830a18a96f86725a275bff85dc552b2
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-09-15 19:36:28 +0600

    add admin,seller,customar route and components

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -12,9 +12,11 @@ import { Link } from 'react-router-dom'
 import SellerMenu from './Menu/SellerMenu'
 import CustomerMenu from './Menu/CustomerMenu'
 import logo from '../../../assets/images/logo-flat.png'
+import useRole from '../../../hooks/useRole'
 const Sidebar = () => {
   const { logOut } = useAuth()
   const [isActive, setActive] = useState(false)
+  const [role, isLoading] = useRole();
 
   // Sidebar Responsive Handler
   const handleToggle = () => {
@@ -71,15 +73,9 @@ const Sidebar = () => {
           <div className='flex flex-col justify-between flex-1 mt-6'>
             <nav>
               {/*  Menu Items */}
-              <CustomerMenu />
-              <SellerMenu />
-
-              <MenuItem
-                icon={BsGraphUp}
-                label='Statistics'
-                address='/dashboard'
-              />
-              <AdminMenu />
+              {role === 'customer' && <CustomerMenu />}
+              {role === 'seller' && <SellerMenu />}
+              {role === 'admin' && <AdminMenu />}
             </nav>
           </div>
         </div>
---
+++ src/hooks/useRole.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import useAxiosSecure from "./useAxiosSecure";
+import useAuth from "./useAuth";
+import { useQuery } from "@tanstack/react-query";
+
+const useRole = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user, loading } = useAuth();
+  const { data: role, isLoading } = useQuery({
+    queryKey: ["role", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/users/role/${user?.email}`);
+      return data.role;
+      console.log(role);
+    },
+  });
+  return [role, isLoading];
+};
+
+export default useRole;
